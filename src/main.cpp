b0e3a355ab0d700df1a76e2558556b29b5b38fbe
rafafelps <rafaelfelipe@alunos.utfpr.edu.br>
2023-07-18 14:22:47 -0300

    Higher accuracy checker

--- src/main.cpp
+++ src/main.cpp
@@ -46,57 +46,16 @@ int main(int argc, char* argv[]) {
             if (lastLayer[i] > lastLayer[highVal]) { highVal = i; }
         }
 
-        if (label != highVal) {
-            continue;
-            unsigned int width = testData.getWidth();
-            unsigned int height = testData.getHeight();
-            libattopng_t* png = libattopng_new(width, height, PNG_GRAYSCALE);
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    libattopng_put_pixel(png, testData.img[image]->values[i * width + j] * 255);
-                }
-            }
-            for (int i = 0; i < outputSize; i++) {
-                if (label != i) {
-                    std::cout << std::fixed << std::setprecision(3) << i << ": " << lastLayer[i] * 100 << "%" << std::endl;
-                } else {
-                    std::cout << std::fixed << std::setprecision(3) << i << ": " << lastLayer[i] * 100 << "% <-" << std::endl;
-                }
-            }
-            libattopng_save(png, "images/result.png");
-            libattopng_destroy(png);
-        } else { correctPredictions++; }
+        if (label == highVal) { correctPredictions++; }
 
         delete[] lastLayer;
     }
 
-    std::cout << std::endl << "Accuracy: " << correctPredictions / static_cast<float>(dataSize) << std::endl;
+    float acc = correctPredictions / static_cast<float>(dataSize);
+    std::cout << "Accuracy: " << acc << std::endl;
 
-    /*unsigned int pos = 3;
-    unsigned int label = data.img[pos]->label;
-    mnist.propagate(data.img[pos]->values);
-
-    unsigned int width = data.getWidth();
-    unsigned int height = data.getHeight();
-    libattopng_t* png = libattopng_new(width, height, PNG_GRAYSCALE);
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            libattopng_put_pixel(png, data.img[pos]->values[i * width + j] * 255);
-        }
+    if (acc > atof(argv[2])) {
+        std::cout << "Higher accuracy found!" << std::endl;
+        return 0;
     }
-    libattopng_save(png, "images/result.png");
-    libattopng_destroy(png);
-
-    float* lastLayer = mnist.getResults();
-    for (int i = 0; i < outputSize; i++) {
-        if (label != i) {
-            std::cout << std::fixed << std::setprecision(3) << i << ": " << lastLayer[i] * 100 << "%" << std::endl;
-        } else {
-            std::cout << std::fixed << std::setprecision(3) << i << ": " << lastLayer[i] * 100 << "% <-" << std::endl;
-        }
-    }
-
-    delete[] lastLayer;*/
-    
-    return 0;
 }
